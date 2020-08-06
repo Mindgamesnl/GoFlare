a3c2891bd65f83c2f5db59b880c0fd5d9dc87d48
Mindgamesnl <matsmoolhuizen@gmail.com>
2020-08-06 21:10:48 +0200

    Unfuck

--- cloudflare.go
+++ cloudflare.go
@@ -29,86 +29,86 @@ func NewCloudFlare(email string, key string) CloudFlare {
 	}
 }
 
-func (cf CloudFlare) GetZoneInfo(domain string) ZoneInfoResponse {
-	response := ZoneInfoResponse{}
-	cf.doBasicRequest("GET", "https://api.cloudflare.com/client/v4/zones?name=" + domain, response)
+func (cf CloudFlare) GetZoneInfo(domain string) *ZoneInfoResponse {
+	response := &ZoneInfoResponse{}
+	cf.doBasicRequest("GET", "https://api.cloudflare.com/client/v4/zones?name="+domain, response)
 	return response
 }
 
 func (cf CloudFlare) GetUserProfile() UserProfile {
-	response := UserProfile{}
+	response := &UserProfile{}
 	cf.doBasicRequest("GET", "https://api.cloudflare.com/client/v4/user", response)
-	return response
+	return *response
 }
 
 func (cf CloudFlare) RegisterZone(domain string) ZoneRegisterResponse {
-	response := ZoneRegisterResponse{}
+	response := &ZoneRegisterResponse{}
 	cf.doHeaderRequest("POST", "https://api.cloudflare.com/client/v4/zones", response, map[string]string{
 		"name": domain,
 	})
-	return response
+	return *response
 }
 
 func (cf CloudFlare) DeleteZone(domain string) ZoneDeleteResponse {
-	response := ZoneDeleteResponse{}
-	cf.doBasicRequest("DELETE", "https://api.cloudflare.com/client/v4/zones/" + domain, response)
-	return response
+	response := &ZoneDeleteResponse{}
+	cf.doBasicRequest("DELETE", "https://api.cloudflare.com/client/v4/zones/"+domain, response)
+	return *response
 }
 
 func (cf CloudFlare) SetZoneDevelopmentMode(domain string, inDevelopment bool) ZoneDevModeResponse {
-	response := ZoneDevModeResponse{}
-	cf.doHeaderRequest("PATH", "https://api.cloudflare.com/client/v4/zones/" + domain + "/settings/development_mode", response, map[string]string{
+	response := &ZoneDevModeResponse{}
+	cf.doHeaderRequest("PATH", "https://api.cloudflare.com/client/v4/zones/"+domain+"/settings/development_mode", response, map[string]string{
 		"value": strconv.FormatBool(inDevelopment),
 	})
-	return response
+	return *response
 }
 
 func (cf CloudFlare) GetZoneDevelopmentMode(domain string) ZoneDevModeResponse {
-	response := ZoneDevModeResponse{}
-	cf.doBasicRequest("GET", "https://api.cloudflare.com/client/v4/zones/" + domain + "/settings/development_mode", response)
-	return response
+	response := &ZoneDevModeResponse{}
+	cf.doBasicRequest("GET", "https://api.cloudflare.com/client/v4/zones/"+domain+"/settings/development_mode", response)
+	return *response
 }
 
-func (cf CloudFlare) ZoneDnsList(domain string) ZoneDnsList {
-	response := ZoneDnsList{}
-	cf.doBasicRequest("GET", "https://api.cloudflare.com/client/v4/zones/" + domain + "/dns_records", response)
-	return response
+func (cf CloudFlare) ZoneDnsList(id string) ZoneDnsList {
+	response := &ZoneDnsList{}
+	cf.doBasicRequest("GET", "https://api.cloudflare.com/client/v4/zones/"+id+"/dns_records?per_page=100", response)
+	return *response
 }
 
 func (cf CloudFlare) AddDns(domain string, recordType string, content string, name string, proxied bool) ZoneDnsAddedResponse {
-	response := ZoneDnsAddedResponse{}
-	cf.doHeaderRequest("POST", "https://api.cloudflare.com/client/v4/zones/" + domain + "/dns_records", response, map[string]string{
-		"type": recordType,
-		"name": name,
+	response := &ZoneDnsAddedResponse{}
+	cf.doHeaderRequest("POST", "https://api.cloudflare.com/client/v4/zones/"+domain+"/dns_records", response, map[string]string{
+		"type":    recordType,
+		"name":    name,
 		"content": content,
 		"proxied": strconv.FormatBool(proxied),
 	})
 
-	return response
+	return *response
 }
 
 func (cf CloudFlare) ListWorkers(profile UserProfile) WorkerListResponse {
-	response := WorkerListResponse{}
-	cf.doBasicRequest("GET", "https://api.cloudflare.com/client/v4/accounts/" + profile.Result.ID + "/workers/scripts", response)
-	return response
+	response := &WorkerListResponse{}
+	cf.doBasicRequest("GET", "https://api.cloudflare.com/client/v4/accounts/"+profile.Result.ID+"/workers/scripts", response)
+	return *response
 }
 
 func (cf CloudFlare) UploadWorker(profile UserProfile, worker Worker, javaScript string) WorkerUploadResponse {
-	response := WorkerUploadResponse{}
-	cf.doUploadRequest("PUT", "https://api.cloudflare.com/client/v4/accounts/" + profile.Result.ID + "/workers/scripts/" + worker.Name, response, map[string]string{
+	response := &WorkerUploadResponse{}
+	cf.doUploadRequest("PUT", "https://api.cloudflare.com/client/v4/accounts/"+profile.Result.ID+"/workers/scripts/"+worker.Name, response, map[string]string{
 		"Content-Type": "application/javascript",
 	}, javaScript)
-	return response
+	return *response
 }
 
 func (cf CloudFlare) DownloadWorker(profile UserProfile, worker Worker) string {
-	return cf.doRequest("GET", "https://api.cloudflare.com/client/v4/accounts/" + profile.Result.ID + "/workers/scripts/" + worker.Name, nil, map[string]string{
+	return cf.doRequest("GET", "https://api.cloudflare.com/client/v4/accounts/"+profile.Result.ID+"/workers/scripts/"+worker.Name, nil, map[string]string{
 		"Accept": "application/javascript",
 	}, nil)
 }
 
-func (cf CloudFlare) doBasicRequest(how string, endpoint string, what interface{}) {
-	cf.doRequest(how, endpoint, what, emptyMap, nil)
+func (cf CloudFlare) doBasicRequest(how string, endpoint string, what interface{}) interface{} {
+	return cf.doRequest(how, endpoint, what, emptyMap, nil)
 }
 
 func (cf CloudFlare) doHeaderRequest(how string, endpoint string, what interface{}, headers map[string]string) {
@@ -122,6 +122,7 @@ func (cf CloudFlare) doUploadRequest(how string, endpoint string, what interface
 func (cf CloudFlare) doRequest(how string, endpoint string, what interface{}, values map[string]string, body io.Reader) string {
 	client := http.Client{}
 	request, err := http.NewRequest(how, endpoint, body)
+	request.Header.Add("Authorization", "Bearer " + cf.ApiKey)
 	request.Header.Add("X-Auth-Key", cf.ApiKey)
 	request.Header.Add("X-Auth-Email", cf.Email)
 
@@ -138,10 +139,12 @@ func (cf CloudFlare) doRequest(how string, endpoint string, what interface{}, va
 		log.Fatalln(err)
 	}
 
+	rb, _ := ioutil.ReadAll(resp.Body)
+	responseBody := string(rb)
+
 	if what != nil {
-		json.NewDecoder(resp.Body).Decode(&what)
+		json.Unmarshal([]byte(responseBody), &what)
 	}
 
-	resposneBody, _ := ioutil.ReadAll(resp.Body)
-	return string(resposneBody)
+	return responseBody
 }
